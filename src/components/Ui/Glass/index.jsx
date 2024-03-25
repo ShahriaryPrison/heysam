fcd3caaf140eaafed617d5cef1bb8c41b0ab89d4
Seyyed Sadrodin Bigdeli <sadra.bigdeli1350@gmail.com>
2024-03-26 00:33:01 +0330

    fix bug redirect dashboarad

--- src/components/Ui/Glass/index.jsx
+++ src/components/Ui/Glass/index.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
-function index() {
+function Index() {
   return (
-    <div>index</div>
-  )
+    <div class="glass p-10">
+      <h1 class="text-3xl text-white">یک دیو شیشه‌ای با Tailwind CSS</h1>
+      <p class="text-lg text-white mt-4">این یک مثال ساده است.</p>
+    </div>
+  );
 }
 
-export default index
+export default Index;
