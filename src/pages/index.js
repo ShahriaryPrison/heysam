9a12103a23d046a4436212657896915ba4de7798
Seyyed Sadrodin Bigdeli <sadra.bigdeli1350@gmail.com>
2024-03-26 03:56:07 +0330

    coding skill section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,13 @@
-import SkillsTag from "@/components/modules/SkillsTag";
-import Glass from "@/components/modules/Glass";
 import Header from "@/components/modules/Header";
 import MainAboutUs from "@/components/modules/MainAboutUs";
 import MemberBox from "@/components/modules/MemberBox";
+import Skills from "@/components/template/Skills";
 export default function Home() {
   return (
     <section>
       <Header />
       <MainAboutUs />
-      <SkillsTag />
+      <Skills />
       <MemberBox/>
     </section>
   );
