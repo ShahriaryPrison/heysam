ab34d19293d5a4de08dfa8ebe2b303b5e81f362d
Seyyed Sadrodin Bigdeli <sadra.bigdeli1350@gmail.com>
2024-03-27 05:20:56 +0330

    coding team member

--- src/components/modules/MemberBox.jsx
+++ src/components/modules/MemberBox.jsx
@@ -1,27 +1,39 @@
 import Image from "next/image";
 import React from "react";
 
-function MemberBox({ img }) {
+function MemberBox({ name, tech, gitHubLink, image }) {
   return (
-    <div className="flex flex-col p-8 glass w-full max-w-72 rounded-[40px] gap-14 text-white">
+    <div className="glass flex flex-col justify-between p-8 glass w-full max-w-72 min-h-96 rounded-[40px] gap-14 text-white">
       <div className="w-full flex justify-center items-center">
-        <div className="rounded-full bg-slate-600 w-36 h-36">
-          {/* <Image
-          src={img}
-          className="w-full"
-          width="100"
-          height="100"
-          alt="user image"
-        /> */}
-        </div>
+        {image ? (
+          <div className="member-shadow rounded-full bg-slate-600 w-36 h-36 overflow-hidden">
+            <Image
+              src={image}
+              className="w-full h-full"
+              width="100"
+              height="100"
+              alt="user image"
+            />
+          </div>
+        ) : (
+          <div className="member-shadow rounded-full bg-[#E8E8E8] bg-opacity-5 w-36 h-36"></div>
+        )}
       </div>
-      <div className="flex gap-4 flex-col">
-        <div className="flex justify-between border-b py-1">
-          <span>name</span>
-          <span>side</span>
+      <div className="flex gap-6 flex-col">
+        <div className="flex justify-between border-b py-2">
+          <span className="text-nowrap text-xs flex items-end">{name}</span>
+          <span className="text-nowrap text-sm flex items-end">{tech}</span>
         </div>
         <div className="flex justify-center ">
-            <div className=" px-4 py-1 rounded-xl border border-white">show in GitHub</div>
+          <a
+            href={gitHubLink}
+            className=" px-4 py-1.5 rounded-xl border border-white flex items-center hover:opacity-40 transition"
+          >
+            <span className="text-nowrap flex items-center">
+
+            Visit GitHub
+            </span>
+          </a>
         </div>
       </div>
     </div>
--- src/components/template/Skills.jsx
+++ src/components/template/Skills.jsx
@@ -106,11 +106,12 @@ function Skills() {
   return (
     <section id="skills" className="w-full flex flex-col gap-36 py-10">
       <h4 className="text-white w-full text-center text-3xl">Skills</h4>
-      <div className="w-full max-w-7xl grid grid-cols-6 justify-center items-center mx-auto grid-rows-3 gap-8">
+      <div className="relative w-full max-w-7xl grid grid-cols-6 justify-center items-center mx-auto grid-rows-3 gap-8">
         {skillsData.map((skill) => (
             <SkillsTag key={skill.id} skill={skill.skill} image={skill.skillImage} />
 
         ))}
+        <div className="w-full h-52 shrink-0 rounded-[448px] bg-gradient-to-r from-cyan-500 to-fuchsia-500  opacity-[0.34] blur-[100px] absolute right-0" />
       </div>
     </section>
   );
