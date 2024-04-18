cd19e29f6642438881242b1e93d0c92665799640
Seyyed Sadrodin Bigdeli <sadra.bigdeli1350@gmail.com>
2024-04-18 21:27:45 +0330

    handle email

--- src/components/template/Header.jsx
+++ src/components/template/Header.jsx
@@ -3,16 +3,16 @@ import Logo from "../../../public/images/heysam-logo-no-back.png";
 import Link from "next/link";
 function Header({content , langState}) {
   return (
-    <header className="flex justify-between items-center w-full py-4 px-6 custom-header-dir">
-      <a href="" className="flex justify-center w-full lg:w-min  items-center px-8 cursor-pointer">
+    <header className="flex justify-between items-center w-full px-2 py-2 sm:py-4 sm:px-6 custom-header-dir">
+      <a href="" className="flex justify-center sm:w-full sm:max-w-40 lg:w-min  items-center px-6 sm:px-8 cursor-pointer">
         <Image
           src={Logo}
-          className="w-11 h-11"
+          className="w-8 h-8 sm:w-11 sm:h-11"
           width="80"
           height="80"
           alt="Logo"
         />
-        <p className="text-gradient font-[Poppins] font-bold text-3xl">EYSAM</p>
+        <p className="text-gradient font-[Poppins] font-bold text-lg sm:text-3xl">EYSAM</p>
       </a>
       <ul className="hidden lg:flex gap-6 ">
         <a href="#about_us">
@@ -30,11 +30,11 @@ function Header({content , langState}) {
           <li className={`text-[#BCBCBC] list-item-hover ${langState === "fa" && "text-lg font-bold"}`}>{content.contact_us}</li>
         </a>
       </ul>
-      <div className="hidden lg:block">
-        <Link href={"/en"} className={`px-6 py-3 text-[#BCBCBC] ${langState === "en" && "border border-[#D7D7D7] rounded-lg"}`}>
+      <div className="py-4">
+        <Link href={"/en"} className={`px-3 sm:px-6 py-2 sm:py-3 text-sm sm:text-base text-[#BCBCBC] ${langState === "en" && "border border-[#D7D7D7] rounded-lg"}`}>
           English
         </Link>
-        <Link href={"/fa"} className={`px-6 py-3 text-[#BCBCBC] ${langState === "fa" && "border border-[#D7D7D7] rounded-lg"}`}>فارسی</Link>
+        <Link href={"/fa"} className={`px-3 sm:px-6 py-2 sm:py-3 text-sm sm:text-base text-[#BCBCBC] ${langState === "fa" && "border border-[#D7D7D7] rounded-lg"}`}>فارسی</Link>
       </div>
     </header>
   );
