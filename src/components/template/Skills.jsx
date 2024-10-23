acfdea4eec31a9c17b25ed1f3ca899e3bbdae2c4
Seyyed Sadrodin Bigdeli <sadra.bigdeli1350@gmail.com>
2024-10-23 21:30:02 +0330

    change sy photo

--- src/components/template/Skills.jsx
+++ src/components/template/Skills.jsx
@@ -3,9 +3,9 @@ import skillsData from "@/data/skillData";
 
 function Skills({content}) {
   return (
-    <section id="skills" className="w-full flex flex-col gap-36 py-10 px-8 lg:px-16">
+    <section id="skills" className="w-full max-w-7xl flex flex-col gap-36 py-10">
       <h4 className="text-white w-full text-center text-3xl">{content.title}</h4>
-      <div className="relative w-full grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-6 justify-center items-center gap-8 place-content-center place-items-center">
+      <div className="relative w-full grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 justify-center items-center gap-8 place-content-center place-items-center">
         {skillsData.map((skill) => (
           <SkillsTag
             key={skill.id}
