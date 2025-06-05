9ffd3b1983637156619559c41b18c8c04e140699
MShJSTM <gmodern2560@gmail.com>
2025-06-05 13:17:17 +0330

    google tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,23 +1,25 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className="scroll-smooth">
         <Head>
-          <Script
+          {/* Google Analytics */}
+          <script
+            async
             src="https://www.googletagmanager.com/gtag/js?id=G-SHPP4800J2"
-            strategy="afterInteractive"
-          />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', 'G-SHPP4800J2');
-            `}
-          </Script>
+            `,
+            }}
+          />
         </Head>
         <body>
           <Main />
