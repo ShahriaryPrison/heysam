faf1a4c9769480fb65a60210e1af2c2edd5ef6c2
MShJSTM <gmodern2560@gmail.com>
2025-06-05 12:23:13 +0330

    robot

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,23 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en" className='scroll-smooth'>
+      <Html lang="en" className="scroll-smooth">
         <Head>
-     
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-SHPP4800J2"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-SHPP4800J2');
+            `}
+          </Script>
         </Head>
         <body>
           <Main />
